d36b4c1fa15b72ca1a89ca4293091b8f59af1e05
Martín <martinnadereduardo@gmail.com>
2025-09-08 11:28:57 -0300

    Continué con el Modulo 5

---
+++ Modulo 5 - Estructuras de datos/ListaConNumeros.py
@@ -0,0 +1,31 @@
+# Crear una lista con al menos 5 números
+
+notasExamen = [6, 7, 5, 9, 10]
+
+# Mostrar el primero elemento, el del medio y el ultimo
+
+print("El primer elemento de la lista es:", notasExamen[0])
+print("El elemento del medio de la lisa es:", notasExamen[2])
+print("El ultimo elementeo de la lista es:", notasExamen[-1])    # El "-1" se usa para mostrar el ultimo elemento de la lista
+
+# cambiar el valor del segundo elemento
+
+notasExamen[4] = 8       # El numero dentro del corchete indica la posición del elemento a cambiar
+
+# Agrega un nuevo numero al final
+
+notasExamen.append(7)
+
+# Elimina el tercer numero
+
+notasExamen.remove(5)
+
+# Imprimir la lista final
+
+print("Lista completa:", notasExamen)
+
+
+
+
+
+
