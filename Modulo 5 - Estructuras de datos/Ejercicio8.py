73b1696ec0eb097eb78e0d6c452ec5ce9ae7150c
MartinNaderPlay <martinnadereduardo@gmail.com>
2025-10-26 13:39:39 -0300

    Agregando el primer ejercicio de tuplas

---
+++ Modulo 5 - Estructuras de datos/Ejercicio8.py
@@ -0,0 +1,8 @@
+# Realizar una tupla con 5 frutas 
+# Mostrar el primer y ultimo elemento de la tupla
+# Mostrar el tamaño de la tupla con len()
+
+tuplaFrutas = ("Manzana", "Naranja", "Banana", "Kiwi", "Ciruela")
+
+print("El primer elemento de la tupla es:", tuplaFrutas[0])
+print("La cantidad de elementos de la tupla es:", len(tuplaFrutas))
