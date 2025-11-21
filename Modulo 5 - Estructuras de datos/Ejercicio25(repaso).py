a94f2f1e17c66d665ee490e3d5bfe97d8cbfd87a
Martín Nader <martinnadereduardo@gmail.com>
2025-11-21 10:30:08 -0300

    Agrego el primer ejercicio de repaso del modulo 5

---
+++ Modulo 5 - Estructuras de datos/Ejercicio25(repaso).py
@@ -0,0 +1,10 @@
+# Lista de compras:
+# Crea una lista con 5 alimentos. Agrega uno más al final. imprime la lista
+
+listaCompras = ["Aceitunas", "Harina", "Pan", "Tomate", "Galletas"]
+
+listaCompras.append("Azucar")
+
+print("Lista de compras:")
+for valor in listaCompras:
+    print(valor)
