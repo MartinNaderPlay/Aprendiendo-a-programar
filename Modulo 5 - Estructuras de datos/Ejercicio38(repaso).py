d3a13b3803e1761d867f73d2f07dca16489a4ff9
Martín Nader <martinnadereduardo@gmail.com>
2025-11-26 11:24:29 -0300

    Agrego otro ejercicio

--- Modulo 5 - Estructuras de datos/Ejercicio38(repaso).py
+++ Modulo 5 - Estructuras de datos/Ejercicio38(repaso).py
@@ -1,3 +1,11 @@
 # Buscar en lista
 # Escribe una función que busque un número dentro de una lista. Devuelve True o False.
 
+listaNumeros = [2, 6, 7, 9, 15]
+
+numeroIngreso = [int(input("Escribe un numero para buscar en la lista: "))]
+
+setNumeros = set(listaNumeros)
+setNumeroIngreso = set(numeroIngreso)
+
+print(setNumeroIngreso.issubset(setNumeros))
