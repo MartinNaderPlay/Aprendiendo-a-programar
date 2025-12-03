970019661f54cbe0251a83c90fa6590ac7a5d4e2
Martín Nader <martinnadereduardo@gmail.com>
2025-12-03 09:36:38 -0300

    Ejercicio 55 corregido

--- Modulo 5 - Estructuras de datos/Ejercicio55(repaso).py
+++ Modulo 5 - Estructuras de datos/Ejercicio55(repaso).py
@@ -8,32 +8,27 @@ def agregar(nombre, numero):
     lista_contactos[nombre] = numero
     print()
     
-
 def borrar(nombre):
     del lista_contactos[nombre]
     print()
     
-
 def buscar(nombre):
     print(f"El numero de telefono de '{nombre}' es: {lista_contactos[nombre]}")
     print()
     
-
 def mostrar():
     print("Lista de contactos\n------------------")
     for nombre, numero in lista_contactos.items():
         print(f"{nombre}: {numero}")
     print()    
     
-
-print()
-decision = str(input("Desea realizar alguna función? (si/no): "))
 print()
 
-while decision == "si":
+while str(input("Desea realizar alguna función? (si/no): ")) == "si":
     
     funcion = int(input("Qué función desea realizar?\n\nPara agregar ingrese (1)...\nPara borrar ingrese (2)...\nPara buscar ingrese (3)...\nPara ver la lista completa ingrese (4)...\n\nEntrada: "))
     print()
+
     if funcion == 1:
         agregar(str(input("ingrese el nombre del contacto: ")), int(input("Ingrese su numero: ")))
     elif funcion == 2:
@@ -42,6 +37,9 @@ while decision == "si":
         buscar(str(input("Ingrese el nombre del contacto que busca: ")))
     elif funcion == 4:
         mostrar()
+    else:
+        print("Entrada incorrecta.\n")
+
 print("El programa ha finalizado.")
 
 
