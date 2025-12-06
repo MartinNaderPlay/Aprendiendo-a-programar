8688d084946a43806fa191a7ee927afed2edb145
MartinNaderPlay <martinnadereduardo@gmail.com>
2025-12-06 09:43:08 -0300

    agrego el ejercicio 57

--- Modulo 5 - Estructuras de datos/Ejercicio57(repaso).py
+++ Modulo 5 - Estructuras de datos/Ejercicio57(repaso).py
@@ -6,15 +6,37 @@ lista_tareas = []
 n=0
 
 def agregar(tarea):
-    lista_tareas[{}]
+    lista_tareas.append({})
     lista_tareas[n]["Tarea"] = tarea
+    lista_tareas[n]["Estado"] = "Pendiente"
 
-def buscar(tarea):
-    
+def modificar(tarea, estado):
+    for elemento in lista_tareas:
+        if elemento["Tarea"] == tarea:
+            elemento["Estado"] = estado
+
+def mostrar():
+    print("---------------------------")
+    for elemento in lista_tareas:
+        for tarea, estado in elemento.items():
+            print(f"{tarea}: {estado}")
+        print("---------------------------")
+
+def eliminar(tarea):
+    for elemento in lista_tareas:
+        if elemento["Tarea"] == tarea:
+            lista_tareas.remove(elemento)
 
 
+while str(input("Desea agregar una tarea? (si/no): ")) == "si":
+    agregar(str(input("Ingrese la tarea que quiere agregar: ")))
+    n+=1
 
+mostrar()
 
-
-
+modificar(str(input("Ingrese la tarea que desea marcar... ")), str(input("Ingrese el estado en que está la tarea... ")))
    
+eliminar(str(input("Ingrese la tarea que desea eliminar... ")))   
+
+mostrar()
+
