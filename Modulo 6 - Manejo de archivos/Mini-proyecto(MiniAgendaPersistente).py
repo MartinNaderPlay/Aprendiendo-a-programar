4ec3180bbb9a399133e9b87bae912aaa3f4336aa
Martín Nader <martinnadereduardo@gmail.com>
2025-12-11 15:09:13 -0300

    comienzo con el modulo 6

---
+++ Modulo 6 - Manejo de archivos/Mini-proyecto(MiniAgendaPersistente).py
@@ -0,0 +1,13 @@
+# Crear un programa capaz de:
+# Guardar contactos en un archivo JSON
+# Cargar los contactos al iniciar
+# Agregar nuevos contactos
+# Listarlos
+# Eliminar uno
+# Salir
+
+
+import json
+
+contactos = {}
+
