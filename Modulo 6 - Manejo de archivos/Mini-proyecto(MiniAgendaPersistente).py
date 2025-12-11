41844bf9fe28c4b73da2db95b1ff704757f7cbf3
MARTIN EDUARDO NADER <martin@MacBook-Pro-de-MARTIN.local>
2025-12-11 18:29:41 -0300

    Agrego avance del mini-proyecto

--- Modulo 6 - Manejo de archivos/Mini-proyecto(MiniAgendaPersistente).py
+++ Modulo 6 - Manejo de archivos/Mini-proyecto(MiniAgendaPersistente).py
@@ -9,5 +9,20 @@
 
 import json
 
-contactos = {}
+contactos = {
+    "Martín" : 3865351457,
+    "Roberto" : 3865425592,
+    "Angela" : 381361568
+}
 
+with open("contactos.json", "w", encoding="utf-8") as f:
+    json.dump(contactos, f, indent=4)
+
+
+with open("contactos.json", "r", encoding="utf-8") as f:
+    lista = json.load(f)
+
+lista["Germán"] = 11365890
+
+with open("contactos.json", "w", encoding="utf-8") as f:
+    json.dump(lista, f, indent=4)
